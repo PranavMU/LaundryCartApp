1c012ccb0713ecf1822ae9d585efaa544004709f
Balu_Kandru <swamy.kandru@gmail.com>
2022-07-27 16:45:46 +0530

    updated schema

--- server/modals/user.js
+++ server/modals/user.js
@@ -2,19 +2,39 @@ const mongoose=require("mongoose")
 const Schema=mongoose.Schema
 
 const userschema= new Schema({
-    name : {type:String, required:true},
-    email:{type:String, required:true,unique:true},
-    phone :{type :String, required:true,unique:true},
-    address: [
-        {
-            addressType:{type:String,default:"Home"},
-            streetAddress :{type:String, required:true},
-            state :{type:String, required:true},
-            district :{type:String, required:true},
-            pincode :{type:Number, required:true},
-        }
-    ],
-    password:{type :String, required:true}
+    name : {
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone :{
+        type :String,
+        required:true,unique:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    password:{
+        type :String,
+        required:true
+    },
+    state:{
+        type:String,
+        required:true
+    },
+    district:{
+        type:String,
+        required:true
+    },
+    pincode:{
+        type:String,
+        required:true
+    }
 })
 
 const usermodal=mongoose.model("users",userschema)
