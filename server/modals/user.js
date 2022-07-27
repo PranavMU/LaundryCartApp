6ce90f9fe6922c6bedec749595fafb875ffbd442
PranavMU <102609201+PranavMU@users.noreply.github.com>
2022-07-27 15:49:36 +0530

    user-modal

---
+++ server/modals/user.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose")
+const Schema=mongoose.Schema
+
+const userschema= new Schema({
+    name : {type:String, required:true},
+    email:{type:String, required:true,unique:true},
+    phone :{type :String, required:true,unique:true},
+    address: [
+        {
+            addressType:{type:String,default:"Home"},
+            streetAddress :{type:String, required:true},
+            state :{type:String, required:true},
+            district :{type:String, required:true},
+            pincode :{type:Number, required:true},
+        }
+    ],
+    password:{type :String, required:true}
+})
+
+const usermodal=mongoose.model("users",userschema)
+
+module.exports=usermodal
--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-//testing
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+const usermodal=require("./modals/user")
+
+const cors=require("cors")
+var bodyParser = require('body-parser')
+app.use(cors())
+
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.DATABASE)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT,()=>{
+    console.log("server started @ : " +process.env.PORT)
+})
