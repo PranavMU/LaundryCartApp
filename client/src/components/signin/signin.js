1e320506e98faf447a9472ad8f0506de05527fe0
Gokulakannan.M <gokulakannan.m@GokulakannanMs-MacBook-Air.local>
2022-07-28 11:07:03 +0530

    signinpart

--- client/src/components/signin/signin.js
+++ client/src/components/signin/signin.js
@@ -1,11 +1,50 @@
 import { Header } from "../header/header";
+import padlock from "../assests/padlock.svg";
 import "../signin/signin.css";
 const Signin = ()=>{
     return(
         <>
         <div className="signin-container">
         <Header/>
-        <h1>signin</h1>
+        <section className="left-section-container">
+            <div>
+                <p className="core-text">Laundry Service</p>
+                <p className="slogan">Doorstep Wash & Dryclean Service</p>
+            </div>
+            <div className="da-1">
+                <p className="da-12">Don't have An Account?</p>
+            </div>
+            <div className="button-container">
+            <button className="register-button">Register</button>
+
+            </div>
+        </section>
+        <section className="signin-container-2">
+            <div className="signin-card">
+                <div className="signin-text">
+                    <p className="signin-12">SIGN IN</p>
+                </div>
+                <div className="vl"></div>
+                <div className="input-section">
+                        <input id="email" placeholder="Mobile/Email"/>
+                        <hr className="hr1"></hr>
+                </div>
+                <div className="password-section">
+                    <input id="password" placeholder="Password"/>
+                    <hr className="hr2"></hr>
+                </div>
+                <div className="forgot-password">
+                    <p className="pass12">Forgot Password?</p>
+                </div>
+            </div>
+            <div className="signin-button-container">
+            <button className="signin-button">Sign In</button>
+            <div className="padlock-container">
+                <img src={padlock} alt="err"></img>
+            </div>
+            </div>
+
+        </section>
 
         </div>
         </>
