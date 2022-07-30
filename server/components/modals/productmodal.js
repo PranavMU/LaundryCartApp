d9bd9dc394fe413e146a52115133ff5422198818
PranavMU <102609201+PranavMU@users.noreply.github.com>
2022-07-30 12:10:58 +0530

    productmodal

--- server/components/modals/productmodal.js
+++ server/components/modals/productmodal.js
@@ -1,7 +1,16 @@
-// const mongoose =require("mongoose")
+const mongoose =require("mongoose")
 
-// const productModel= new mongoose.Schema({
-//     type:[{
+const productschema= new mongoose.Schema({
+    productType : {
+        type:String, required:true
+    },
+    description : {type:String, required:true
+    },
+    wash : {type:Number, required:true,default:20},
+    press : {type:Number, required:true,default:10},
+    drywash : {type:Number, required:true,default:10},
+    chemwash : {type:Number, required:true,default:20},
+})
+const productmodal=mongoose.model("products",productschema)
 
-//     }]
-// })
+module.exports=productmodal
