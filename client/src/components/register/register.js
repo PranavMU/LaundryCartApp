0849012af8407faf3c20da91d0cc6ddd77d76776
Gokulakannan.M <gokulakannan.m@GokulakannanMs-MacBook-Air.local>
2022-07-30 08:49:43 +0530

    changes in client

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -1,6 +1,14 @@
 import { Header } from "../header/header";
+import Referral from "../referral/referral";
+import Aboutus from "../aboutusfooter/aboutus";
+import Footer from "../footer/footer";
+import { useNavigate } from "react-router-dom";
 import "../register/register.css";
 const Register = ()=>{
+    const navigate = useNavigate();
+    const gotosignin = ()=>{
+        navigate("/");
+    }
     return(
         <>
         <div className="register-container">
@@ -14,7 +22,7 @@ const Register = ()=>{
             <p className="da-21">Don't have An Account?</p>
         </div>
         <div className="register-button-sontainer">
-            <button className="register-signin-button">Sign In</button>
+            <button className="register-signin-button" onClick={gotosignin}>Sign In</button>
         </div>
         </section>
         <section className="right-register-container">
@@ -24,44 +32,46 @@ const Register = ()=>{
             <div className="vl1"></div>
             <form>
                 <div className="register-name-input-section">
-                    <input id="name" type="text" placeholder="Name" className="register-name-input-section-name"/>
+                    <input id="name1" type="text" placeholder="Name" className="register-name-input-section-name"/>
                 <hr className="hr3"></hr>
                 </div>
                  <div className="register-email-input-section">
-                    <input id="email" type="email" placeholder="Email" className="register-email-input-section-email"/>
+                    <input id="email1" type="email1" placeholder="Email" className="register-email-input-section-email"/>
                     <hr className="hr4"></hr>   
                 </div>
-                {/*<div className="register-phone-input-section">
-                    <input id="phone" type="text" placeholder="Phone"/>
+                <div className="register-phone-input-section">
+                    <input id="phone1" type="text" placeholder="Phone" className="register-phone-input-section-section-phone"/>
                     <hr className="hr5"></hr>   
                 </div>
                 <div className="register-state-input-section">
-                    <input id="state" type="text" placeholder="State"/>
+                    <input id="state" type="text" placeholder="State" className="register-state-input-section-state"/>
                     <hr className="hr6"></hr>   
                 </div>
                 <div className="register-district-input-section">
-                    <input id="district" type="text" placeholder="District"/>
+                    <input id="district" type="text" placeholder="District" className="register-state-input-section-district"/>
                     <hr className="hr7"></hr>   
                 </div>
                 <div className="register-address-input-section">
-                    <input id="address" type="text" placeholder="Address"/>
+                    <input id="address" type="text" placeholder="Address" className="register-address-input-section-address"/>
                     <hr className="hr8"></hr>   
                     </div>
-                    <div className="register-pincode-input-section">
-                    <input id="pincode" type="text" placeholder="Pincode"/>
+                <div className="register-pincode-input-section">
+                    <input id="pincode" type="text" placeholder="Pincode" className="register-pincode-input-section-pincode"/>
                     <hr className="hr9"></hr>   
                 </div>
                 <div className="register-checkbox-input-section">
                     <input  id="checkbox-register" type="checkbox"/>
-                    <label htmlFor="checkbox-register">I agree to Terms & Condition receiving marketing and promotional materials</label>
-                </div> */}
+                    <label htmlFor="checkbox-register" className="checkbox-text">I agree to Terms & Condition receiving marketing and promotional materials</label>
+                </div>
             </form>
             <div className="register-button-container">
 
             <button type="submit" className="register-button-input-section">Register</button>
             </div>
         </section>
-
+        <Referral/>
+        <Aboutus/>
+        <Footer/>
         </div>
         </>
     )
